52f77eaf59b0b47308ad4e2e68a7354fa1cdc291
boolfalse <boolfalse@mail.ru>
2022-01-16 23:14:37 +0400

    generate controller (nest g controller item)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemController } from './item/item.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ItemController],
   providers: [AppService],
 })
 export class AppModule {}
