658f6c5843ab4d8dc773d31e1cb94f4faed84353
boolfalse <boolfalse@mail.ru>
2022-01-17 01:09:21 +0400

    .gitignore updates; @nestjs/config usage for .env; Connect with MongoDB Atlas via Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemController } from './item/item.controller';
 import { ItemService } from './item/item.service';
+import { ItemModule } from './item/item.module';
+import { MongooseModule} from "@nestjs/mongoose";
+import { ConfigModule } from '@nestjs/config'; // TODO: create separated config module
 
 @Module({
-  imports: [],
+  imports: [ItemModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController, ItemController],
   providers: [AppService, ItemService],
 })
