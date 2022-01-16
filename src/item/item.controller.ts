cb56e437e8d065c61991b1eafa6ed16136e656f5
boolfalse <boolfalse@mail.ru>
2022-01-16 23:29:34 +0400

    initial methods, initial DTO

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Post, Put, Delete} from '@nestjs/common';
 
 @Controller('item')
-export class ItemController {}
+export class ItemController {
+    @Get()
+    findAll(): string {
+        return 'This action returns all items';
+    }
+    @Post()
+    create(): string {
+        return 'This action adds an item';
+    }
+    @Get(':id')
+    findOne(id: string): string {
+        return `This action returns a #${id} item`;
+    }
+    @Put(':id')
+    update(id: string): string {
+        return `This action updates a #${id} item`;
+    }
+    @Delete(':id')
+    remove(id: string): string {
+        return `This action removes a #${id} item`;
+    }
+}
