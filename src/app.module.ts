6754cfea2866ec1c258b5e2c40182dfd72da1500
boolfalse <boolfalse@mail.ru>
2022-01-17 02:01:57 +0400

    Fix for: Nest can't resolve dependencies of the ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemController } from './item/item.controller';
-import { ItemService } from './item/item.service';
+// import { ItemController } from './item/item.controller';
+// import { ItemService } from './item/item.service';
 import { ItemModule } from './item/item.module';
 import { MongooseModule} from "@nestjs/mongoose";
 import { ConfigModule } from '@nestjs/config'; // TODO: create separated config module
 
 @Module({
   imports: [ItemModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI)],
-  controllers: [AppController, ItemController],
-  providers: [AppService, ItemService],
+  controllers: [AppController], // , ItemController
+  providers: [AppService], // , ItemService
 })
 export class AppModule {}
